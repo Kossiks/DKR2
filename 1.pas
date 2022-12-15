d88b940a1b138422a063b750f540e1e35914f175
Kossiks <117629075+Kossiks@users.noreply.github.com>
2022-12-15 17:27:01 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,17 @@
+﻿begin
+  var min,max,i:integer;
+  var n:=readinteger('Введите количество элементов массива');
+  var a:array of integer;
+  SetLength(a,n);
+  for i:=0 to n-1 do
+    a[i]:= readinteger;
+    min:=a[1];
+    max:=a[1];
+    for i:=0 to n-1 do
+    begin
+      if a[i]<min then min:=a[i];
+      if a[i]>max then max:=a[i];
+    end;
+    print('Максимальный элемент = ', max);
+    print('Минимальный элемент = ', min);
+  end.
---
+++ 2.pas
@@ -0,0 +1,19 @@
+﻿begin
+var s: string;
+var f: boolean;
+var a, i: byte; 
+s:=readstring;
+begin
+    a := length(s); 
+    f := True; 
+    for i := 1 to a div 2 do
+        if s[i] <> s[a-i+1] then begin
+            f := False;
+            break;
+        end; 
+    if f = True then
+        print('Является палиндромом')
+    else
+        print('Не является палиндромом');
+    end;
+end.
